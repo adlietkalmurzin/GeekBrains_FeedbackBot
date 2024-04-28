1eee769dc5d46e2400f7ed3f595d595f473adde7
mishkashishka333 <74455994+MurzaevMisha@users.noreply.github.com>
2024-04-28 10:53:39 +0400

    Update identify_object.py

--- configs/models/identify_object/identify_object.py
+++ configs/models/identify_object/identify_object.py
@@ -1,3 +1,30 @@
+import numpy as np
+import torch 
+
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+
+import warnings
+warnings.simplefilter('ignore')
+
+
+
+tokenizer = AutoTokenizer.from_pretrained("configs/models/identify_object/tokenizer")
+id3label = {0: "вебинар", 1: "программа", 2: "преподаватель"}
+label3id = {"вебинар": 0, "программа": 1, 'преподаватель': 2}
+model_object = AutoModelForSequenceClassification.from_pretrained(
+    "configs/models/identify_object/model_object_1", num_labels=3, id2label=id3label, label2id=label3id, ignore_mismatched_sizes=True
+)
+
 def identify_object(text):
-    object_ = 0
-    return object_
+    l = [0,0,0]
+
+    for i in text:
+        s = tokenizer.encode(i, return_tensors='pt')
+        x = model_object(s).logits[0].detach().numpy()
+        x = np.exp(x)/sum(np.exp(x))
+        l[0] += x[0]
+        l[1] += x[1]
+        l[2] += x[2]
+    print('obj', l)
+    return int(np.argmax(l))
